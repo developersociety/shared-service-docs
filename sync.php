ae2feb45657f59a9060b6b7fc6ffea5c6e1beda5
Stephen Hawkes <steve@dev.ngo>
2021-04-17 19:59:50 +0100

    Update theming for fonts and logo

--- sync.php
+++ sync.php
@@ -36,13 +36,23 @@ $index = <<<INDEX
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Catalyst Shared Service Layer documentation</title>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="description" content="Helping charities build digital skills and improve their services.">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css" title="Simple">
-  <link href="https://assets-global.website-files.com/6048aaba41858594b67e1803/6048aaba4185854c837e184e_cat-fav.png" rel="shortcut icon" type="image/x-icon"/>
+    <meta charset="UTF-8">
+    <title>Catalyst Shared Service Layer documentation</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="description" content="Helping charities build digital skills and improve their services.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css" title="Simple">
+    <link href="https://assets-global.website-files.com/6048aaba41858594b67e1803/6048aaba4185854c837e184e_cat-fav.png" rel="shortcut icon" type="image/x-icon"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsify-themeable@0/dist/css/theme-simple.css">
+    <!-- Custom theme styles -->
+    <style>
+      :root {
+        --base-font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+        --base-color: #58536e;
+      }
+    </style>
 </head>
 <body>
   <div id="app"></div>
@@ -69,5 +79,6 @@ INDEX;
 
 file_put_contents('docs/.nojekyll', '');
 file_put_contents('docs/_sidebar.md', $sidebar);
-file_put_contents('docs/README.md', '# Docs');
+file_put_contents('docs/README.md', '# Shared service elements');
 file_put_contents('docs/index.html', $index);
+exec('cp catalyst.svg docs/catalyst.svg');
