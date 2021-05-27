e70b3a9eb5cd4bb9a5d4e424e9ae9d53c3c64be1
GitHub Action <action@github.com>
2021-05-27 01:29:42 +0000

    Sync

---
+++ docs/referrals-ui/Services/PendingOrgs/IPendingOrgsService.cs
@@ -0,0 +1,17 @@
+﻿using OpenReferralPOV.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Services.PendingOrgs
+{
+    public interface IPendingOrgsService
+    {
+        Task<IEnumerable<Organization>> GetPendingOrgs();
+        Task<Organization> CreatePendingOrg(Organization organization);
+        Task VerifyPendingOrganisation(string orgId);
+        Task<bool> UserHasPendingOrg();
+        Task DeletePendingOrganisation(string orgId);
+    }
+}
